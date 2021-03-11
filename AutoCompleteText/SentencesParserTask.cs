ed7627da783bfd6fb8b5a9c0b3f21b9374803d1a
shabanin <shabanin@PC403-10>
2021-03-11 10:42:28 +0300

    govna

--- AutoCompleteText/SentencesParserTask.cs
+++ AutoCompleteText/SentencesParserTask.cs
@@ -7,7 +7,17 @@ namespace TextAnalysis
         public static List<List<string>> ParseSentences(string text)
         {
             var sentencesList = new List<List<string>>();
-            //...
+            string[] sentences = text.Split('.', '!', ';', ':', '(', ')');
+            for(int i=0;i<sentences.Length;i++)
+            {
+                string []words = sentences[i].Split(',', ' ');
+                var wordslist = new List<string>();
+                for(int j =0;j<words.Length;j++)
+                {
+                    wordslist.Add(words[j]);
+                }
+                sentencesList.Add(wordslist);
+            }
             return sentencesList;
         }
     }
