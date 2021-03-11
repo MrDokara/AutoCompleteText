c179dc132b927f5e79a6f29b8bd88a46151a3544
sA1mon <loner40466@gmail.com>
2021-03-11 10:37:51 +0300

    Почти все

--- AutoCompleteText/FrequencyAnalysisTask.cs
+++ AutoCompleteText/FrequencyAnalysisTask.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace TextAnalysis
 {
@@ -11,29 +12,44 @@ namespace TextAnalysis
 
             foreach (var sentences in text)
             {
-                for (int current = 1; current < sentences.Count; current++)
+                for (int i = 1; i < sentences.Count; i++)
                 {
-                    var previous = current - 1;
+                    var j = i - 1;
+                    var currentWord = sentences[i];
+                    var previousWord = sentences[j];
 
-                    if (!frequencyDictionary.ContainsKey(sentences[previous]))
-                        frequencyDictionary[sentences[previous]] = new Dictionary<string, int>();
-
-                    if (!frequencyDictionary[sentences[previous]].ContainsKey(sentences[current]))
-                        frequencyDictionary[sentences[previous]].Add(sentences[current], 0);
-
-                    frequencyDictionary[sentences[previous]][sentences[current]]++;
+                    UpdateDictionary(frequencyDictionary, previousWord, currentWord);
                 }
 
-                for (int current = 2; current < sentences.Count; current++)
+                for (int i = 2; i < sentences.Count; i++)
                 {
-                    var previous = current - 1;
-                    var preprevious = previous - 1;
+                    var j = i - 1;
+                    var k = j - 1;
 
+                    var currentWord = sentences[i];
+                    var previousWord = sentences[j];
+                    var prepreviousWord = sentences[k];
 
+                    var twoWords = $"{prepreviousWord} {previousWord}";
+
+                    UpdateDictionary(frequencyDictionary, twoWords, currentWord);
                 }
             }
 
+            foreach (var )
+
             return result;
+        }
+
+        private static void UpdateDictionary(Dictionary<string, Dictionary<string, int>> frequencyDictionary, string previousWord, string currentWord)
+        {
+            if (!frequencyDictionary.ContainsKey(previousWord))
+                frequencyDictionary[previousWord] = new Dictionary<string, int>();
+
+            if (!frequencyDictionary[previousWord].ContainsKey(currentWord))
+                frequencyDictionary[previousWord].Add(currentWord, 0);
+
+            frequencyDictionary[previousWord][currentWord]++;
         }
    }
 }
