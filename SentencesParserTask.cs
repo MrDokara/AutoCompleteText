5a137167a70289cfbdd8ad5594e88c6127877f9d
shabanin <shabanin@PC403-10>
2021-03-18 09:50:33 +0300

    Sdelal

--- SentencesParserTask.cs
+++ SentencesParserTask.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace TextAnalysis
 {
@@ -7,17 +8,31 @@ namespace TextAnalysis
         public static List<List<string>> ParseSentences(string text)
         {
             var sentencesList = new List<List<string>>();
-            string[] sentences = text.Split('.', '!', ';', ':', '(', ')');
-            for(int i=0;i<sentences.Length;i++)
-            {
-                string []words = sentences[i].Split(',', ' ');
-                var wordslist = new List<string>();
-                for(int j =0;j<words.Length;j++)
+            string[] sentences = text.Split('.', '!', ';', ':', '(', ')', '?');
+ 
+                for (int i = 0; i < sentences.Length; i++)
                 {
-                    wordslist.Add(words[j]);
+                    string[] words = sentences[i].Split(',', ' ', '^', '#', '$', '-', '+', '1', '=', ' ', '\t', '\n', '\r');
+                    var wordslist = new List<string>();
+                    if (sentences[i] != "")
+                    {
+                        for (int j = 0; j < words.Length; j++)
+                        {
+                            if (words[j] != "")
+                            {
+                                if (char.IsUpper(words[j][0]) == true)
+                                {
+                                    StringBuilder sb = new StringBuilder(words[j]);
+                                    sb[0] = char.ToLower(words[j][0]);
+                                    wordslist.Add(sb.ToString());
+                                }
+                                else
+                                    wordslist.Add(words[j]);
+                            }
+                        }
+                        sentencesList.Add(wordslist);
+                    }
                 }
-                sentencesList.Add(wordslist);
-            }
             return sentencesList;
         }
     }
